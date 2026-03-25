d7953cb5bbe58bd841771884de409a34f52d196d
erinbryant <86887887+erinbryant@users.noreply.github.com>
2026-03-25 15:05:50 -0500

    added home

---
+++ src/pages/home.jsx
@@ -0,0 +1,33 @@
+import { useNavigate } from 'react-router-dom'
+import Layout from './Layout'
+
+export default function Home() {
+  const navigate = useNavigate();
+
+  return (
+    <Layout buttonLabel="Login" buttonHref="/login">
+
+      {/* ── PAGE TITLE ── */}
+      <h2>Welcome to Post Office 8</h2>
+      <p>Please select how you are logging in.</p>
+
+      {/* ── NAVIGATION BUTTONS ──
+      TEMPORARY: Using to test other pages directing. Will be routed through login once set up */}
+      <div className="button-group">
+        <button
+          className="btn"
+          onClick={() => navigate('/customer_home')}
+        >
+          Customer
+        </button>
+        <button
+          className="btn"
+          onClick={() => navigate('/employee_home')}
+        >
+          Employee
+        </button>
+      </div>
+
+    </Layout>
+  )
+}
